f4a55e0a58a87e220106185485fc8f372340ac1c
Stepler-Programist <fakilgorit@gmail.com>
2025-05-05 20:54:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const slider = document.querySelector('.slider');
+const sliderARL = document.querySelector('.arrow-left');
+const sliderARR = document.querySelector('.arrow-right');
+const slides = document.querySelector(".slider-image");
+const bottom = document.querySelector('.bottom');
+const top = document.querySelector('.top');
